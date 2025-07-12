3d669663ace695adef9ff7ba3b7497068be04bf4
Hayden Brehm <haydenbrehm92@gmail.com>
2025-07-11 21:02:20 -0500

    Initial commit for pacman sprite assets (pacman, ghosts, level, etc.)
    added system exit function & pg.display.update

--- run_game.py
+++ run_game.py
@@ -1,27 +1,23 @@
 # Example file showing a basic pygame "game loop"
 import pygame as pg
+from sys import exit
 
 # pygame setup
 pg.init()
 screen = pg.display.set_mode((1280, 720))
 clock = pg.time.Clock()
-running = True
 
-while running:
+while True:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            exit()
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
 
-    # flip() the display to put your work on screen
-    pg.display.flip()
-
+    pg.display.update()
     clock.tick(60)  # limits FPS to 60
-
-pg.quit()
